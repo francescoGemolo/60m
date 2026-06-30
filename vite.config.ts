432f5fc21862a99131518159de5664b685d61ee6
Francesco <francescolavoro0209@gmail.com>
2026-06-30 15:28:24 +0200

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/60/",
+  base: "/60m/",
   resolve: {
     alias: {
       "lottie-web": "lottie-web/build/player/lottie_light",
